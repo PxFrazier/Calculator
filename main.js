2d856f8207302f81abbd4421a01fdbb0a7b15f55
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-10-06 14:30:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+const display = document.getElementById('output');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+var query = String();
+var element = String();
+
+document.getElementById('equals').addEventListener('click', ()=>{
+    query += element;
+    display.innerText = math.evaluate(query);
+    query = '';
+    element = '';
+});
+document.getElementById('clearEntry').addEventListener('click', ()=>{
+    element = '';
+    display.innerText = '';
+});
+document.getElementById('clearOutput').addEventListener('click', ()=>{
+    display.innerText = '';
+    query = '';
+});
+document.getElementById('negative').addEventListener('click', ()=>{
+    if(element == '') element += '-';
+    else element += '(-';
+
+    display.innerText += element;
+});
+document.getElementById('sqrt').addEventListener('click', ()=>{
+    display.innerText = Math.sqrt(parseFloat(display.innerText));
+    query = '';
+    element = '';
+});
+
+numbers.forEach((entry)=>{
+    entry.addEventListener('click', ()=>{
+        element += entry.innerText;
+        display.innerText += entry.innerText;
+    });
+});
+
+operators.forEach((entry)=>{
+    entry.addEventListener('click', ()=>{
+        element += entry.innerText;
+        query += element;
+        element = '';
+        display.innerText += entry.innerText;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
